5ec1ee836fa5e58240938a8b36141c5f78d3dd9c
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-10-19 03:27:32 +0100

    sign up page complemented with sign up form

--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,9 +1,49 @@
+import { useState } from 'react'
+
+// styles 
 import styles from './SignUp.module.css'
 
 export default function SignUp() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  } 
+
   return (
-    <div>
-        Sign Up      
-    </div>
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+        <h2>Sign Up</h2>
+        <label>
+          <span>email:</span>
+          <input 
+            type="text"
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email}
+          />
+        </label>
+
+        <label>
+          <span>password:</span>
+          <input 
+            type="password"
+            onChange={(e) => setPassword(e.target.value)} 
+            value={password}
+          />
+        </label>
+
+        <label>
+          <span>username:</span>
+          <input 
+            type="text"
+            onChange={(e) => setUsername(e.target.value)} 
+            value={username}
+          />
+        </label>
+
+        <button className="btn">signup</button>
+    </form>
   )
 }
